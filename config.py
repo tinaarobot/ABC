f2ddbaae980c3d5bc759722a1dfcaecbeb6eb59f
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-01 16:56:36 +0530

    Update config.py
    Signed-off-by: tinaarobot <146160310+tinaarobot@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/c1e38928238bdd9715914.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/e65b4d164d980041e5b9a.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/c1e38928238bdd9715914.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/e65b4d164d980041e5b9a.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/c1e38928238bdd9715914.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/c1e38928238bdd9715914.jpg", "https://telegra.ph/file/e65b4d164d980041e5b9a.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
