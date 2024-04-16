cfac265cf770d17dfc547efd5fcbf3a255b49f38
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-16 12:20:19 +0530

    Update play.py
    Signed-off-by: 『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,7 +1,7 @@
 import math
-
+from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-
+from AnonXMusic import app
 from AnonXMusic.utils.formatters import time_to_seconds
 
 
@@ -33,40 +33,38 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "༗—————————"
+        bar = "Ⓐ▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—༗————————"
+        bar = "▰Ⓐ▱▱▱▱▱▱▱▰"
     elif 20 <= umm < 30:
-        bar = "——༗———————"
+        bar = "▰▱Ⓐ▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———༗——————"
+        bar = "▰▱▱Ⓐ▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————༗—————"
+        bar = "▰▱▱▱Ⓐ▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "—————༗————"
+        bar = "▰▱▱▱▱Ⓐ▱▱▱▰"
     elif 60 <= umm < 70:
-        bar = "——————༗———"
+        bar = "▰▱▱▱▱▱Ⓐ▱▱▰"
     elif 70 <= umm < 80:
-        bar = "———————༗——"
+        bar = "▰▱▱▱▱▱▱Ⓐ▱▰"
     elif 80 <= umm < 95:
-        bar = "————————༗—"
+        bar = "▰▱▱▱▱▱▱▱Ⓐ▰"
     else:
-        bar = "—————————༗"
+        bar = "▰▱▱▱▱▱▱▱▰Ⓐ"
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        
+        [
+            InlineKeyboardButton(text="◁◁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -75,13 +73,11 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="◁◁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        
+        ],  
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -92,11 +88,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -156,4 +152,5 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
-    
+
+                    
