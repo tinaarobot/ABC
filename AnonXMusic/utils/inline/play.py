de14272691b85d717f0966742d51b2812c0615ce
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-16 20:36:24 +0530

    Update play.py
    Signed-off-by: 『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -60,20 +60,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="◁◁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="◯", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴜᴘᴅᴀᴛᴇ",
-                url=f"https://t.me/roy_editx",
-            ),
-            InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ",
-                url=f"https://t.me/the_friendz",
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -83,20 +73,10 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="◁◁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="◯", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴜᴘᴅᴀᴛᴇ",
-                url=f"https://t.me/roy_editx",
-            ),
-            InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ",
-                url=f"https://t.me/the_friendz",
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
