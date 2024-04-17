b44c8c3b8feae680e988f85ee6479cac235c7cca
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-17 13:20:14 +0530

    Update inline.py

--- AnonXMusic/plugins/bot/inline.py
+++ AnonXMusic/plugins/bot/inline.py
@@ -36,22 +36,21 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʏᴏᴜᴛᴜʙᴇ 🎄",
+                            text="ʏᴏᴜᴛᴜʙᴇ",
                             url=link,
                         )
                     ],
                 ]
             )
             searched_text = f"""
-❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
+❖ <b>ᴛɪᴛʟᴇ ➥</b> <a href={link}>{title}</a>
 
-⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
-👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
-🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
-⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
+● <b>ᴅᴜʀᴀᴛɪᴏɴ ➥</b> {duration} ᴍɪɴᴜᴛᴇs
+● <b>ᴠɪᴇᴡs ➥</b> <code>{views}</code>
+● <b>ᴄʜᴀɴɴᴇʟ ➥</b> <a href={channellink}>{channel}</a>
+● <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ ➥</b> {published}
 
-
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
+<b>❖ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ ➥ {app.name}</b>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
