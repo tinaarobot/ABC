2c403a5ad540c6a772a9096b63ba9d455f09bc89
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-17 11:35:38 +0530

    Create wall.py

---
+++ AnonXMusic/plugins/tools/wall.py
@@ -0,0 +1,48 @@
+from pyrogram import Client, filters
+import requests
+from pyrogram.types import Message
+from io import BytesIO
+from AnonXMusic import app
+
+# Fill these out with your credentials
+
+
+def get_random_picture():
+    response = requests.get('https://source.unsplash.com/random')
+    if response.status_code == 200:
+        return BytesIO(response.content)
+    else:
+        return None  # If something went wrong
+
+
+# Command handler to respond to /pic commands
+@app.on_message(filters.command("wall"))
+def pic(client, message):
+    random_pic = get_random_picture()
+    if random_pic:
+        message.reply_photo(random_pic)
+    else:
+        message.reply("✦ Sorry, I couldn't get a random picture at the moment. 😔")
+
+
+@app.on_message(filters.command("pic"))
+def pic_command(client, message: Message):
+    # Extract the name from the command
+    try:
+        name = message.command[1]
+    except IndexError:
+        client.send_message(message.chat.id, "✦ Please provide a name after the /pic command.")
+        return
+
+    # Build the Unsplash URL with the provided name
+    unsplash_url = f"https://source.unsplash.com/500x500/?{name}"
+
+    # Send the image as a photo
+    try:
+        response = requests.get(unsplash_url)
+        if response.status_code == 200:
+            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"✦ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➠ {name}.\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐" )
+        else:
+            client.send_message(message.chat.id, "✦ Failed to fetch image.")
+    except requests.RequestException as e:
+        client.send_message(message.chat.id, f"✦ An error occurred ➠ {str(e)}")        
