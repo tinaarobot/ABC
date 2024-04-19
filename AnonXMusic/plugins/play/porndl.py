21734faaa3065c0a9b24d06dfa672dfdd74a6c2d
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-19 13:22:38 +0530

    Update porndl.py

--- AnonXMusic/plugins/play/porndl.py
+++ AnonXMusic/plugins/play/porndl.py
@@ -1,30 +1,30 @@
+
 from pyrogram import filters
 import requests, random
 from bs4 import BeautifulSoup
 from AnonXMusic import app
 import pytgcalls
 import os, yt_dlp 
-from pyrogram.types import CallbackQuery, Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from pytgcalls.types import AudioVideoPiped
-#from AnonXMusic.plugins.play import play
-#from AnonXMusic.plugins.play.porndl import play
 
-######
+####
+
 
 keyboard = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close_data"), 
+            InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close_data"),       
         ]
 ])
 
-#######
-       
+##########
+
 @app.on_callback_query(filters.regex("^close_data"))
 async def close_callback(_, query):
     chat_id = query.message.chat.id
     await query.message.delete()
 
-########  
+##
 
 def get_video_info(title):
     url_base = f'https://www.xnxx.com/search/{title}'
@@ -47,22 +47,12 @@ def get_video_info(title):
     return None
 
 
-@app.on_message(filters.command("porn"))
-async def get_random_video_info(client, message):
- # if len(message.command) == 1:
-       # await message.reply("❖ Please provide a title to search.")
-      # return
-     
-             if message.chat.type != ChatType.PRIVATE:
-        return await message.reply("**❍ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ᴜsᴀʙʟᴇ ɪɴ ᴘᴍ ғᴏʀ ɢʀᴏᴜᴘ ᴘʀᴏᴛᴇᴄᴛɪᴏɴ.**",
-         reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("ɢᴏ ᴘᴍ", url=f"https://t.me/{app.me.username}?start=True")]
-           ]
-        ))
 
-       # await message.reply("❖ Please provide a title to search.")
-        #return
+@app.on_message(filters.command("xporn"))
+async def get_random_video_info(client, message):
+    if len(message.command) == 1:
+        await message.reply("Please provide a title to search.")
+        return
 
     title = ' '.join(message.command[1:])
     video_info = get_video_info(title)
@@ -70,9 +60,9 @@ async def get_random_video_info(client, message):
     if video_info:
         video_link = video_info['link']
         video = await get_video_stream(video_link)
-        await message.reply_video(video, caption=f"❖ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇᴀʀᴄʜ ➥ {title}", reply_markup=keyboard)
+        await message.reply_video(video, caption=f"{title}", reply_markup=keyboard)
              
     else:
-        await message.reply(f"❖ No video link found for ➥ {title}")
+        await message.reply(f"No video link found for '{title}'.")
 
-
+######
