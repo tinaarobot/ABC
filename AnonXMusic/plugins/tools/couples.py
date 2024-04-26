ff2bfaad92c9f01d311f444563fa0579788b9f7f
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-27 01:23:39 +0530

    Update couples.py

--- AnonXMusic/plugins/tools/couples.py
+++ AnonXMusic/plugins/tools/couples.py
@@ -100,7 +100,7 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (81, 215), img1)
+         img.paste(img1, (70, 200), img1)
          img.paste(img2, (950, 215), img2)
 
          img.save(f'test_{cid}.png')
