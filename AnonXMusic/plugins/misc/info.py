447c694cf9333640629f0816018228bdacb8d6ac
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-27 11:10:17 +0530

    Update info.py

--- AnonXMusic/plugins/misc/info.py
+++ AnonXMusic/plugins/misc/info.py
@@ -42,7 +42,7 @@ async def get_userinfo_img(
         circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
         circular_img.paste(img, (0, 0), mask)
         resized = circular_img.resize((355, 355))
-        bg.paste(resized, (540, 70), resized)
+        bg.paste(resized, (530, 60), resized)
 
     img_draw = ImageDraw.Draw(bg)
 
