a88cd9d3f4b6d66877bb86afdd7738d5c4fba08c
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-04-29 20:53:21 +0530

    Fixed All ✅

--- AnonXMusic/plugins/tools/Welcome.py
+++ AnonXMusic/plugins/tools/Welcome.py
@@ -106,7 +106,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
     except AttributeError:
-        pic = "assets/upic.png"
+        pic = "AnonXMusic/assets/upic.png"
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
