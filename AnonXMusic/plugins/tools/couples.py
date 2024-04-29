b20b95cb76c143972c68a317f9209b18fdf4e3fc
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-04-29 11:26:30 +0530

    Update couples.py

--- AnonXMusic/plugins/tools/couples.py
+++ AnonXMusic/plugins/tools/couples.py
@@ -100,8 +100,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (103, 234), img1)
-         img.paste(img2, (1235, 234), img2)
+         img.paste(img1, (100, 238), img1)
+         img.paste(img2, (1210, 238), img2)
 
          img.save(f'test_{cid}.png')
     
